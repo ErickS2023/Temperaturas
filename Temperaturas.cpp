b0434be15bbcf3bdacfb96130b4b6b41e138534d
ErickS2023 <141955911+ErickS2023@users.noreply.github.com>
2023-08-17 12:35:55 -0600

    temperaturas

---
+++ Temperaturas.cpp
@@ -0,0 +1,20 @@
+#include  <stdio.h>
+#Fahrenheid(9/5.o)*celcius + 32
+#Celcius (Fahrenheid-32)* (5/9)
+int main(){
+	
+   float (temperatura) = ;
+  
+   printf("temperatura Fahrenheid") (9/.50)* ("celcius")+32
+  
+   printf("temperatura Celcius") ("Fahrenheid -32.0")*(5.0/9.0)
+  
+   printf("mostrar temperatura fahrenheid")
+  
+   printf("mostrar temperatura celcius")
+  
+   printf("regresar 0")
+  
+	return 0;
+}
+
